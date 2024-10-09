6a56ebb9c6315e40cf6116033891eca76f06db30
champika ranaweera <cranaweera96@gmail.com>
2024-10-09 23:05:55 +0530

    income,expense,total

--- expense-tracker/src/components/Transaction.js
+++ expense-tracker/src/components/Transaction.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0? '-' : '+';
+
   return (
-    <li className = "minus">
-        {transaction.text} <span>-$400</span><button className ="delete-btn">X</button>
+    <li className = {transaction.amount < 0? 'minus' : 'plus'}>
+        {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className ="delete-btn">X</button>
     </li>
   )
 }
